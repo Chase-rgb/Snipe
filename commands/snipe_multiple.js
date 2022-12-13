bd68928196720a1999dcf91d419a51b4b34d1fdd
Chase-rgb <wlee002@gmail.com>
2022-12-13 16:12:03 -0500

    Add multiple sniping message formatting

--- commands/snipe_multiple.js
+++ commands/snipe_multiple.js
@@ -1,11 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { addSnipe } = require('../helper/firebase')
+const { snipeOnReact } = require('../helper/snipeOnReact.js')
 const numTargets = 5
 
 const data = new SlashCommandBuilder()
 	.setName('snipe_multiple')
 	.setDescription("Enter list of people being snipe (don't need to select all options)!")
-    .addAttachmentOption(option => option.setName('image').setDescription("Snipe image"))
-    .addUserOption(option => option.setName('target1').setDescription("Target"))
+    .addAttachmentOption(option => option.setName('image').setDescription("Snipe image").setRequired(true))
+    .addUserOption(option => option.setName('target1').setDescription("Target").setRequired(true))
     .addUserOption(option => option.setName('target2').setDescription("Target"))
     .addUserOption(option => option.setName('target3').setDescription("Target"))
     .addUserOption(option => option.setName('target4').setDescription("Target"))
@@ -16,12 +18,16 @@ const data = new SlashCommandBuilder()
     //         .setRequired(true));
 
 execute = async(interaction) => {
-    let users = []
+    let guildID = interaction.guildId
+    let sniperID = interaction.user
+    let targetID = []
+    let image = interaction.options.getAttachment("image")
+
     try {
         for (let i = 0; i < numTargets; i++) {
             const user = interaction.options.getUser('target' + i);
             if (user) {
-                users.push(user)
+                targetID.push(user)
             }
         }  
     } catch (error) {
@@ -30,7 +36,32 @@ execute = async(interaction) => {
     // const user = interaction.options.getUser('target1');
     // console.log(user)
 
-    await interaction.reply('Pong!');
+
+    const message = await interaction.reply({
+        content: `${buildTargetString(targetID)} just got sniped by ${sniperID}`,
+        files: [image],
+        fetchReply: true
+        
+    });
+
+    snipeOnReact(message, sniperID, ...targetID)
 }
 
+
+function buildTargetString(targets) {
+    console.log(targets);
+    if (targets.length == 1) {
+        return `${targets[0]}`
+    } else if (targets.length == 2) {
+        return `${targets[0]} and ${targets[1]}`
+    } else {
+        for (let i = 0; i < targets.length - 1; i++){
+            let currString = ""
+            currString += `${target[i]}, `
+        }
+        currString += `and ${target[targets.length - 1]}`
+        return currString
+    }
+
+}
 module.exports = {data, execute}
